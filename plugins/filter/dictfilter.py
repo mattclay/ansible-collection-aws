a263c3d7af4533d1995062f3865ab6da477652c1
Matt Clay <matt@mystile.com>
2019-11-06 08:51:43 -0800

    Clean up code to pass ansible-test sanity tests.

--- plugins/filter/dictfilter.py
+++ plugins/filter/dictfilter.py
@@ -1,9 +1,12 @@
 # Copyright (C) 2016 Matt Clay <matt@mystile.com>
 # GNU General Public License v3.0+ (see LICENSE.md or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 
 def dictfilter(item, keys):
-    return {k: item[k] for k in item if k in keys}
+    return dict((k, item[k]) for k in item if k in keys)
 
 
 class FilterModule(object):
