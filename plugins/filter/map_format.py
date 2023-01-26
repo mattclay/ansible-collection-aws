6ea4b10e325f86a74cc771e554a51423fbcb2ab3
Matt Clay <matt@mystile.com>
2023-01-26 09:57:47 -0800

    Code cleanup (#13)
    * Remove old Python 2.x boilerplate
    
    * Rename LICENSE since it's not actually markdown
    
    * Fix grammar issues identifed by PyCharm

--- plugins/filter/map_format.py
+++ plugins/filter/map_format.py
@@ -1,10 +1,6 @@
 # Copyright (C) 2017 Matt Clay <matt@mystile.com>
 # GNU General Public License v3.0+ (see LICENSE.md or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
-
 def map_format(item, fmt, *args, **kwargs):
     d = {}
     d.update(item)
--- plugins/modules/lambda_policy.py
+++ plugins/modules/lambda_policy.py
@@ -2,9 +2,6 @@
 # Copyright (C) 2016 Matt Clay <matt@mystile.com>
 # GNU General Public License v3.0+ (see LICENSE.md or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
 DOCUMENTATION = '''
 ---
 module: lambda_policy
