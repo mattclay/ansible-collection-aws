c4ae039ff59a071c7f8ddf4b41c6261f7d9d6524
Matt Clay <matt@mystile.com>
2023-01-26 09:28:42 -0800

    Remove old Python 2.x boilerplate

--- plugins/filter/ec2_filters.py
+++ plugins/filter/ec2_filters.py
@@ -1,10 +1,6 @@
 # Copyright (C) 2016 Matt Clay <matt@mystile.com>
 # GNU General Public License v3.0+ (see LICENSE.md or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
-
 def ec2_az_vpc_subnets(zones, subnet, name):
     return [dict(
         cidr=map_zone_to_subnet(z, subnet),
